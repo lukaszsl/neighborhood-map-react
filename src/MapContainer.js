f89d0a03275b6e4f6c0d9bce1dbb1f265bad58e4
lukaszsl <lukasz.sliczner@interia.pl>
2018-07-23 21:20:39 +0000

    feat: Add InfoWindow to markers

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -11,33 +11,57 @@ const style = {
 export class MapContainer extends Component {
 
 	state = {
+		showingInfoWindow: false,
+		activeMarker: {},
+		selectedPlace: {},
 		places: [
 			{
 				title: 'Harpa - A powerful concert hall and conference centre',
 				name: 'Harpa',
-				position: {lat: 64.150672, lng: -21.932942}
+				position: {lat: 64.150672, lng: -21.932942},
+				address: 'Austurbakki 2, 101 Reykjavík'
 			},
 			{
 				title: 'Hallgrímskirkja - The church is both a parish church and a national sanctuary in Iceland',
 				name: 'Hallgrímskirkja',
-				position: {lat: 64.142232, lng: -21.926547}
+				position: {lat: 64.142232, lng: -21.926547},
+				address: 'Hallgrímstorg 101, 101 Reykjavík'
 			},
 			{
 				title: 'Ráðhús - The city hall of Reykjavik',
 				name: 'Ráðhús',
-				position: {lat: 64.146630, lng: -21.942340}
+				position: {lat: 64.146630, lng: -21.942340},
+				address: 'Tjarnargata 11, 101 Reykjavík'
 			},
 			{
 				title: 'Þjóðminjasafnið - The National Museum of Iceland',
 				name: 'Þjóðminjasafnið',
-				position: {lat: 64.142194, lng: -21.948606}
+				position: {lat: 64.142194, lng: -21.948606},
+				address: 'Suðurgata 41, 101 Reykjavík'
 			},
 			{
 				title: 'Saga Museum - The History Museum with waxwork exhibits',
 				name: 'Saga Museum',
-				position: {lat: 64.153450, lng: -21.951524}
+				position: {lat: 64.153450, lng: -21.951524},
+				address: 'Grandagarður 2, 101 Reykjavík'
 			}
 		]
+	}
+
+	onMarkerClick = (props, marker, e) =>
+	 this.setState({
+		 selectedPlace: props,
+		 activeMarker: marker,
+		 showingInfoWindow: true
+	 })
+
+	onMapClicked = (props) => {
+		if (this.state.showingInfoWindow) {
+			this.setState({
+				showingInfoWindow: false,
+				activeMarker: null
+			})
+		}
 	}
 
   render() {
@@ -50,14 +74,25 @@ export class MapContainer extends Component {
 					lat: 64.144740,
 					lng: -21.941762
 				}}
+				onClick={this.onMapClicked}
 				>
 					{this.state.places.map((place) => (
 						<Marker
 							key={place.name}
 					    title={place.title}
 					    name={place.name}
-					    position={place.position} />
+					    position={place.position}
+							address={place.address}
+							onClick={this.onMarkerClick} />
 					))}
+					<InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+							<p>{this.state.selectedPlace.address}</p>
+            </div>
+        </InfoWindow>
       </Map>
     );
   }
