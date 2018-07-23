24a32b8bcb6162c07ea37c6deba245e471c9589c
lukaszsl <lukasz.sliczner@interia.pl>
2018-07-23 21:41:24 +0000

    style: Style Info Window children

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-
-
-const style = {
-	width: '100%',
-	height: '100%'
-}
-
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
+import './App.css'
 
 export class MapContainer extends Component {
 
@@ -49,11 +43,11 @@ export class MapContainer extends Component {
 	}
 
 	onMarkerClick = (props, marker, e) =>
-	 this.setState({
-		 selectedPlace: props,
-		 activeMarker: marker,
-		 showingInfoWindow: true
-	 })
+		this.setState({
+			selectedPlace: props,
+			activeMarker: marker,
+			showingInfoWindow: true
+		})
 
 	onMapClicked = (props) => {
 		if (this.state.showingInfoWindow) {
@@ -64,12 +58,15 @@ export class MapContainer extends Component {
 		}
 	}
 
-  render() {
-    return (
-      <Map
+	render() {
+		return (
+			<Map
 				google={this.props.google}
 				zoom={14}
-				style={style}
+				style={{
+					width: '100%',
+					height: '100%'
+				}}
 				initialCenter={{
 					lat: 64.144740,
 					lng: -21.941762
@@ -79,25 +76,25 @@ export class MapContainer extends Component {
 					{this.state.places.map((place) => (
 						<Marker
 							key={place.name}
-					    title={place.title}
-					    name={place.name}
-					    position={place.position}
+							title={place.title}
+							name={place.name}
+							position={place.position}
 							address={place.address}
 							onClick={this.onMarkerClick} />
 					))}
 					<InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-							<p>{this.state.selectedPlace.address}</p>
-            </div>
-        </InfoWindow>
-      </Map>
-    );
-  }
+						marker={this.state.activeMarker}
+						visible={this.state.showingInfoWindow}>
+							<div className='info-window'>
+								<h1>{this.state.selectedPlace.name}</h1>
+								<p>{this.state.selectedPlace.address}</p>
+							</div>
+					</InfoWindow>
+			</Map>
+		);
+	}
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBBr0wyroa-2vbN5Xrm3wicZ0HO5aMIdfY'
+	apiKey: 'AIzaSyBBr0wyroa-2vbN5Xrm3wicZ0HO5aMIdfY'
 })(MapContainer)
