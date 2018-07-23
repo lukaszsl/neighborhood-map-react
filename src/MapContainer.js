a501e7fa00860f88088fcd7bf5e14aa2ac3e1299
lukaszsl <lukasz.sliczner@interia.pl>
2018-07-23 21:00:25 +0000

    feat: Create array of places in MapContainer's state

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -9,6 +9,37 @@ const style = {
 
 
 export class MapContainer extends Component {
+
+	state = {
+		places: [
+			{
+				title: 'Harpa - A powerful concert hall and conference centre',
+				name: 'Harpa',
+				position: {lat: 64.150672, lng: -21.932942}
+			},
+			{
+				title: 'Hallgrímskirkja - The church is both a parish church and a national sanctuary in Iceland',
+				name: 'Hallgrímskirkja',
+				position: {lat: 64.142232, lng: -21.926547}
+			},
+			{
+				title: 'Ráðhús - The city hall of Reykjavik',
+				name: 'Ráðhús',
+				position: {lat: 64.146630, lng: -21.942340}
+			},
+			{
+				title: 'Þjóðminjasafnið - The National Museum of Iceland',
+				name: 'Þjóðminjasafnið',
+				position: {lat: 64.142194, lng: -21.948606}
+			},
+			{
+				title: 'Saga Museum - The History Museum with waxwork exhibits',
+				name: 'Saga Museum',
+				position: {lat: 64.153450, lng: -21.951524}
+			}
+		]
+	}
+
   render() {
     return (
       <Map
@@ -20,22 +51,13 @@ export class MapContainer extends Component {
 					lng: -21.941762
 				}}
 				>
-					<Marker
-				    title={'The marker`s title will appear as a tooltip.'}
-				    name={'Harpa'}
-				    position={{lat: 64.150672, lng: -21.932942}} />
-				  <Marker
-				    name={'Hallgrímskirkja'}
-				    position={{lat: 64.142232, lng: -21.926547}} />
-				  <Marker
-				    name={'Ráðhús'}
-				    position={{lat: 64.146630, lng: -21.942340}} />
-				  <Marker
-				    name={'Þjóðminjasafnið'}
-				    position={{lat: 64.142194, lng: -21.948606}} />
-				  <Marker
-				    name={'Saga Museum'}
-				    position={{lat: 64.153450, lng: -21.951524}} />
+					{this.state.places.map((place) => (
+						<Marker
+							key={place.name}
+					    title={place.title}
+					    name={place.name}
+					    position={place.position} />
+					))}
       </Map>
     );
   }
